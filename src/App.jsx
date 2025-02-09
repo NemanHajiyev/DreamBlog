9042f3408e66ab4bef67a10d885d6781a03f24c6
NemanHajiyev <zaraganq@gmail.com>
2025-02-09 20:18:45 +0400

    Navbar Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import Navabr from './Components/Navabr';
+import Home from './Components/Home';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+//Page Container
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
+import Footer from './Components/Footer';
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='App-container'>
+      <CssBaseline />
+      <BrowserRouter>
+        <Navabr />
+        <Container maxWidth="lg">
+          <Routes>
+            <Route path="/" element={<Home />}>
+            </Route>
+          </Routes>
+        </Container>
+        <Footer />
+      </BrowserRouter>
+    </div>
   )
 }
 
---
+++ src/Components/Navabr.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import '../Styles/Navbar.css';
+import { FaSearch } from 'react-icons/fa';
+
+const Navabr = () => {
+    return (
+        <div className='Navbar'>
+            <div className='navbar-top'>
+                <form className='navbar-form'>
+                    <input type="text" placeholder='Search ...' />
+                    <FaSearch />
+                </form>
+                <div className='navbar-logo'>
+                    <h1>DreamBlog</h1>
+                </div>
+                <div className='navbar-subs'>
+                    <button>
+                        Subscribe
+                    </button>
+                </div>
+            </div>
+            <div className='navbar-bottom'>
+                <ul>
+                    <li>Home</li>
+                    <li>Stock Market</li>
+                    <li>Technology</li>
+                    <li>Politics</li>
+                    <li>Automobile</li>
+                    <li>Healt</li>
+                    <li>Interactive Sessions</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Navabr
